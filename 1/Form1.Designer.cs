e00a62c52ca37ed4b89b6e4644f8d0bb57c50623
wwiillllii <wiliberm@gmail.com>
2017-05-13 22:24:18 +0300

    winForms birthdays

---
+++ 1/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace _1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblName = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtPass = new System.Windows.Forms.TextBox();
+            this.btnLogIn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.lblName.Location = new System.Drawing.Point(12, 9);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(122, 25);
+            this.lblName.TabIndex = 0;
+            this.lblName.Text = "User name:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label1.Location = new System.Drawing.Point(12, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Password:";
+            // 
+            // txtName
+            // 
+            this.txtName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtName.Location = new System.Drawing.Point(141, 14);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(131, 20);
+            this.txtName.TabIndex = 2;
+            // 
+            // txtPass
+            // 
+            this.txtPass.Location = new System.Drawing.Point(141, 38);
+            this.txtPass.Name = "txtPass";
+            this.txtPass.PasswordChar = '*';
+            this.txtPass.Size = new System.Drawing.Size(131, 20);
+            this.txtPass.TabIndex = 3;
+            // 
+            // btnLogIn
+            // 
+            this.btnLogIn.Location = new System.Drawing.Point(17, 63);
+            this.btnLogIn.Name = "btnLogIn";
+            this.btnLogIn.Size = new System.Drawing.Size(255, 23);
+            this.btnLogIn.TabIndex = 4;
+            this.btnLogIn.Text = "Log in";
+            this.btnLogIn.UseVisualStyleBackColor = true;
+            this.btnLogIn.Click += new System.EventHandler(this.btnLogIn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 100);
+            this.Controls.Add(this.btnLogIn);
+            this.Controls.Add(this.txtPass);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblName);
+            this.Name = "Form1";
+            this.Text = "Log in";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtPass;
+        private System.Windows.Forms.Button btnLogIn;
+    }
+}
+
---
+++ 1/mainForm.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace _1
+{
+    public partial class mainForm : Form
+    {
+        string userName;
+        public mainForm(string userName)
+        {
+            InitializeComponent();
+            this.userName = userName;
+        }
+
+        private void dtp_ValueChanged(object sender, EventArgs e)
+        {
+            txtUpdate.Text = "";
+            string line;
+            string date = dtp.Text.Substring(0, dtp.Text.Length - 4);
+            string fileName = userName + "BD.txt";
+            if (!File.Exists(fileName)) File.Create(fileName).Close();
+            using(StreamReader sr = new StreamReader(fileName))
+            {
+                while((line = sr.ReadLine()) != null)
+                    if (line.Contains(date))
+                    {
+                        lblBirthday.Text = "Birthday of: " + line.Split(',')[0];
+                        break;
+                    }
+                if (line == null)
+                    lblBirthday.Text = "No one has a birthday on this day.";
+            }
+        }
+
+        private void mainForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            string fileName = userName + "BD.txt";
+            string tempFile = Path.GetTempFileName();
+            string line;
+            string date = dtp.Text.Substring(0, dtp.Text.Length - 4);
+            if (!File.Exists(fileName)) File.Create(fileName).Close();
+            using (StreamWriter sw = new StreamWriter(tempFile))
+            {
+                using (StreamReader sr = new StreamReader(fileName))
+                    while ((line = sr.ReadLine()) != null)
+                        if (!line.Contains(date))
+                            sw.WriteLine(line);
+                // Removing dates is possible by not writing a name in txtUpdate
+                if(!txtUpdate.Text.Trim().Equals(""))
+                    sw.WriteLine(txtUpdate.Text + "," + dtp.Text);
+            }
+            File.Delete(fileName);
+            File.Move(tempFile, fileName);
+            dtp_ValueChanged(sender, e);
+        }
+    }
+}
